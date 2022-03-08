e870af1c407849c141bb7d857c73ac9fc9dca50c
Gabriel Pereira <berkpereira@gmail.com>
2022-03-08 17:15:40 +0000

    Completed exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, noRepetitions) {
+    if (noRepetitions < 0) {
+        return "ERROR";
+    }
+    let outputString = "";
+    for (let i = 1; i <= noRepetitions; i++) {
+        outputString += string;
+    }
+    return outputString;
 };
 
 // Do not edit below this line
